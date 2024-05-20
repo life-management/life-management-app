ffbb45abe28b71bd2212fb9433215dc7120c0143
weewoo123 <samuelhe2014@gmail.com>
2024-05-20 11:22:25 -0500

    linting

--- backend/LmApi/apps.py
+++ backend/LmApi/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LmapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LmApi'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "LmApi"
