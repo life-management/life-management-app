0e5fcc5d344903cc8df019c53a5c9411e6f736d6
weewoo123 <samuelhe2014@gmail.com>
2024-05-20 10:21:03 -0500

    backend testing configs

---
+++ backend/LmApi/tests/test_views.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.path.append("../LmApi")
+
+from django.test import RequestFactory, TestCase
+from LmApi.views import index
+
+
+# Create your tests here.
+class BasicTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_basic(self):
+        request = self.factory.get("")
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
